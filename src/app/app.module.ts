570ac44477244a155283b26bca27c1d18859a57d
Dan Church <dchurch@g2planet.com>
2020-05-20 22:18:32 -0500

    Add GitHub ribbon
    Designed it myself, TYVM.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ResumeModule } from './resume/resume.module';
 
+import { ForkmeComponent } from './forkme/forkme.component';
+
 @NgModule({
     bootstrap: [
         AppComponent,
     ],
     declarations: [
         AppComponent,
+        ForkmeComponent,
     ],
     imports: [
         BrowserModule,
