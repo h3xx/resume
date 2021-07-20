4389d82e22000eac59e3f7669fc369bbdcd217fb
Dan Church <dchurch@nagios.com>
2021-07-20 12:31:11 -0500

    WIP

--- src/app/resume/skills-graph/skills-graph.component.ts
+++ src/app/resume/skills-graph/skills-graph.component.ts
@@ -20,19 +20,37 @@ import * as c3 from 'c3';
 })
 export class SkillsGraphComponent implements AfterViewInit {
 
+    private _chartSelector?: string;
+    @Input() private set chartSelector(val: string) {
+        this._chartSelector = val;
+        this.createChart();
+    } // end set chartSelector()
+
+    private _data?: c3.Data;
+    @Input() private set data(val: c3.Data) {
+        this._data = val;
+        this.createChart();
+    } // end set data()
+
     constructor() {
     } // end constructor()
 
+    private createChart(): void {
+        if (this._chartSelector && this._data) {
+            const chart = c3.generate({
+                bindto: this._chartSelector,
+                data: this._data,
+                // {
+                //     columns: [
+                //         ['data1', 30, 200, 100, 400, 150, 250],
+                //         ['data2', 50, 20, 10, 40, 15, 25],
+                //     ]
+                // }
+            });
+        }
+    } // end createChart()
+
     public ngAfterViewInit(): void {
-        const chart = c3.generate({
-            bindto: 'skills-graph',
-            data: {
-                columns: [
-                    ['data1', 30, 200, 100, 400, 150, 250],
-                    ['data2', 50, 20, 10, 40, 15, 25],
-                ]
-            }
-        });
     } // end ngAfterViewInit()
 
 }
